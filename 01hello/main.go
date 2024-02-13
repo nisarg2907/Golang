55f0330ff361df197a47f46533b2a1fd33849485
Nisarg <nisargshah.ict19@gmail.com>
2024-02-13 13:14:55 +0530

    init first go basics

---
+++ 01hello/main.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main(){
+	var intNum int = 32
+    var floarNum float32 = 4444444.6166;
+    
+	// printResult(intNum,5);
+	printResult(intNum,floarNum)
+	
+}
+func printResult(num1 int,num2 float32){
+	fmt.Println(num1,num2);
+}
