9f2575212ff3459715aa7dac5478e591a3fb5ecb
Nisarg <nisargshah.ict19@gmail.com>
2024-02-13 13:26:15 +0530

    if else,datatypes done

--- 01hello/main.go
+++ 01hello/main.go
@@ -1,11 +1,17 @@
 package main
 import "fmt"
 
+// data types => int ,int16,int32,int63,float32,float64,string,error,bool
 func main(){
-	var intNum int = 32
+	var intNum int = 3
     var floarNum float32 = 4444444.6166;
     
 	// printResult(intNum,5);
+	if(intNum==32){
+		fmt.Println(intNum);
+	}else{
+		fmt.Println(floarNum);
+	}
 	printResult(intNum,floarNum)
 	
 }
